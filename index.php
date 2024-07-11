31de59b19bc47ada4992bcbaa00b6fde1a2e5eac
shafiqalshaar <shafiqalshaar@gmail.com>
2024-07-11 12:25:11 +0300

    Add .env.example file

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+echo "Hello World!";
+echo "\n\r";
+echo $_ENV['VERSION'];
