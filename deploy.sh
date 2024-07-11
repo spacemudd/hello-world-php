136286772d3fd3a3d70f65df4ca21c4a6001973a
shafiqalshaar <shafiqalshaar@gmail.com>
2024-07-12 02:54:53 +0300

    Add comments

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ docker login -u "$ORACLE_CLOUD_USERNAME" -p "$ORACLE_CLOUD_PASSWORD" me-jeddah-1
 docker-compose build backend
 docker tag hello-world-php-backend:latest me-jeddah-1.ocir.io/axjwpjxefzql/hello-world-php/backend
 docker push me-jeddah-1.ocir.io/axjwpjxefzql/hello-world-php/backend:latest
+kubectl rollout restart deployment.apps/hello-world-php
+echo "Deployment completed successfully!"
